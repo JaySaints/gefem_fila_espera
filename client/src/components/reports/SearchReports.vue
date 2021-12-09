18de399baec43081248576b2ca1670bf7643d7e8
Pablo Santos <pablojs2008@hotmail.com>
2021-12-09 11:37:14 -0300

    update layout emit reports

--- client/src/components/reports/SearchReports.vue
+++ client/src/components/reports/SearchReports.vue
@@ -1,36 +1,50 @@
 <template>
-  <v-card
-    height="150"
-    width="500"
-  >
-      <v-card-title>Selecionar Período</v-card-title>
-      <v-container>
-      <v-row>
-        <v-col cols="4">
-          <v-text-field
-                  label="Data início"
-                  type="date"
-                  required
-                ></v-text-field>
-        </v-col>
-        <v-col cols="4">
-          <v-text-field
-                  label="Data Fim"
-                  type="date"
-                  required
-                ></v-text-field>
-        </v-col>
-        <v-col cols="2">
-          <v-btn
-            rounded
-            class="white--text"
-            color="primary"
-          ><v-icon>mdi-magnify</v-icon> Buscar</v-btn>
-        </v-col>
-
-      </v-row>
-      </v-container>
-  </v-card>
+  <v-container>
+  <v-hover v-slot="{ hover }" open-delay="200">
+    <v-card
+      width="500"
+      :elevation="hover ? 16 : 2"
+    >
+      <v-toolbar color="primary" dark>
+        <v-toolbar-title>{{ title_bar }}</v-toolbar-title>
+      </v-toolbar>
+      <v-card-text>
+        <v-layout>
+          <v-flex>
+            <v-text-field
+              v-model="dateStart"
+              :label="field1_label"
+              type="date"
+              required
+            ></v-text-field>
+          </v-flex>
+          <v-flex v-if="field2">
+            <v-text-field
+              v-model="dateEnd"
+              class="ml-5"
+              :label="field2_label"
+              type="date"
+              required
+            ></v-text-field>
+          </v-flex>
+        </v-layout>
+      </v-card-text>
+      <v-card-actions>
+        <slot name="card_action" />
+        <v-spacer></v-spacer>
+        <v-btn
+          rounded
+          dark
+          color="blue"
+          elevation="0"
+          @click="click()"
+        >
+          <v-icon>mdi-magnify</v-icon>Buscar
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-hover>
+  </v-container>
 </template>
 
 <script>
@@ -39,7 +53,24 @@ export default {
   name: '',
   data () {
     return {
-
+      switch1: false,
+      dateStart: '',
+      dateEnd: ''
+    }
+  },
+  props: [
+    'title_bar',
+    'field1_label',
+    'field2_label',
+    'field2'
+  ],
+  methods: {
+    click () {
+      var payload = {
+        start: this.dateStart,
+        end: this.dateEnd
+      }
+      this.$emit('onClickSearch', payload)
     }
   }
 }
--- client/src/view/Reports.vue
+++ client/src/view/Reports.vue
@@ -3,13 +3,28 @@
       <header-bar />
       <v-container fluid >
         <v-row>
-          <v-col cols="12">
-            <search-reports />
+          <v-col></v-col>
+          <v-col cols="12" sm="4">
+            <search-reports
+              :title_bar="'Relátorio Diário'"
+              :field1_label="'Selecione o Dia'"
+              v-on:onClickSearch="dailyReport"
+            />
           </v-col>
+          <v-col cols="12" sm="4">
+            <search-reports
+              :title_bar="'Relátorio por Periodo'"
+              :field1_label="'Data Início'"
+              :field2_label="'Data Fim'"
+              :field2="true"
+              v-on:onClickSearch="periodReport"
+            />
+          </v-col>
+          <v-col></v-col>
         </v-row>
         <v-row>
           <v-col cols="12" sm="12">
-            <list-reports />
+            <list-reports/>
           </v-col>
         </v-row>
       </v-container>
@@ -25,13 +40,23 @@ export default {
   name: '',
   data () {
     return {
-
+      checkbox: false
     }
   },
   components: {
     HeaderBar,
-    SearchReports,
-    ListReports
+    ListReports,
+    SearchReports
+  },
+  methods: {
+    dailyReport (e) {
+      alert('Data: ' + e.start)
+      // console.log('Data ' + e.start)
+    },
+    periodReport (e) {
+      alert('Data inicio: ' + e.start + ' Data fim: ' + e.end)
+      // console.log('Data inicio: ' + e.start + ' Data fim: ' + e.end)
+    }
   }
 }
 </script>
